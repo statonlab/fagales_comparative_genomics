817be0985a3bc8a1e5c08cbf9e14d84990e219af
mestato <mstaton1@utk.edu>
2016-05-04 16:23:35 -0400

    Added comments to code.

--- synmap/code/SynMap_statistics.py
+++ synmap/code/SynMap_statistics.py
@@ -1,6 +1,13 @@
 #!/data/apps/python/3.2.1/bin/python
 import re, sys
 
+##-----------------------------------
+## Authored: Meg Staton
+## Date: 5/4/16
+## Description: Parses DAGChainer output, generating statistics
+##	DAGChainer file should be provided as first command line argument
+##-----------------------------------
+
 merged_file = sys.argv[1]
 print("SynMap Merged DAGChainer Results file is ", merged_file)
 
@@ -9,19 +16,23 @@ scafSetOrg1 = set()
 scafSetOrg2 = set()
 
 ##-----------------------
-## Get scaffolds from file
+## Iterate file and record scaffolds
 with open(merged_file) as f:
 	for line in f:
 		if '||' in line:
 			line = line.rstrip('\n')
 			wordList = line.split()
+
 			org1scaf = wordList[0]
 			scafSetOrg1.add(org1scaf)
+
 			org2scaf = wordList[4]
 			scafSetOrg2.add(org2scaf)
 			#print(org1scaf + ' ' + org2scaf)
 f.close
 
+##-----------------------
+## Print results
 numOrg1scaf = len(scafSetOrg1)
 numOrg2scaf = len(scafSetOrg2)
 
